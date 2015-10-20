c54fc18d2161dea36976e2df426b11afa9816711
JosieKate Cooley <jkcooley@wireless-10-146-3-201.public.utexas.edu>
2015-10-19 19:22:00 -0500

    Runs the motors on the paper plate (one is backwards) and flashes blinky red and blue

--- RASTemplate/Main.c
+++ RASTemplate/Main.c
@@ -1,24 +1,133 @@
+#include "RASTemp.h"
+
+
 #include <RASLib/inc/common.h>
+#include <RASLib/inc/motor.h>
 #include <RASLib/inc/gpio.h>
 #include <RASLib/inc/time.h>
 
-// Blink the LED to show we're on
-tBoolean blink_on = true;
 
-void blink(void) {
-    SetPin(PIN_F3, blink_on);
-    blink_on = !blink_on;
+static tMotor *Motors[4];
+static tBoolean initialized = false;
+
+void initMotors(void) {
+    if (!initialized) {
+      initialized = true;
+      
+      Motors[0] = InitializeServoMotor(PIN_B6, false);
+      Motors[1] = InitializeServoMotor(PIN_B7, false);
+      Motors[2] = InitializeServoMotor(PIN_C4, false);
+      Motors[3] = InitializeServoMotor(PIN_C5, false);
+    }
 }
 
 
-// The 'main' function is the entry point of the program
+tBoolean led_on = true;
+
+void blink(void) {
+    SetPin(PIN_F1, led_on);
+    SetPin(PIN_F2, !led_on);
+
+    led_on = !led_on;
+}
+
 int main(void) {
-    // Initialization code can go here
-    CallEvery(blink, 0, 0.5);
+     CallEvery(blink, 0, 0.25f); 
+     Printf("\nMotor Demo\n");
+     initMotors();
+     runMotor();
+}
+
+
+void runMotor(void){
+	float left = 0, right = 0, speed = .75f, accel = .01f;
+	left = speed;
+	right = -speed;
+	SetMotor(Motors[0], left);
+	SetMotor(Motors[1], right);
+	SetMotor(Motors[2], right);
+	SetMotor(Motors[3], right);
+}
+
+void stopMotors(void){
+	float left = 0, right = 0;
+        SetMotor(Motors[0], left);
+        SetMotor(Motors[1], left);
+        SetMotor(Motors[2], right);
+        SetMotor(Motors[3], right);
+}
+
+void motorDemo(void) {
+    float left = 0, right = 0, speed = 0.75f, accel = 0.01f;
+    char ch;    
+    int i;
+
+    Printf("Press:\n"
+	   "  w-forward\n"
+	   "  s-backward\n"
+	   "  a-left\n"
+	   "  d-right\n"    
+           "  i-slowly forward\n"
+	   "  k-slowly backward\n"
+	   "  j-slowly left\n"
+	   "  l-slowly right\n"    
+           "  space-stop\n"
+	   "  enter-quit\n");
+  
+    // wait for the user to enter a character
+    ch = ' ';
     
-    while (1) {
-        // Runtime code can go here
-        Printf("Hello World!\n");
+    while (ch != '\n') {
+        switch (ch) {
+            case 'w':
+                left = speed;
+                right = speed;
+                break;
+            case 's':
+                left = -speed;
+                right = -speed;
+                break;
+            case 'a':
+                left = -speed;
+                right = speed;
+                break;
+            case 'd':
+                left = speed;
+                right = -speed;
+                break;
+            case 'i':
+                right += accel;
+                left += accel;
+                break;
+            case 'k':
+                right -= accel;
+                left -= accel;
+                break;
+            case 'j':
+                right -= accel;
+                left += accel;
+                break;
+            case 'l':
+                right += accel;
+                left -= accel;
+                break;
+            default:
+                left = 0; 
+                right = 0;
+                break;
+        }
+
+        SetMotor(Motors[0], left);
+        SetMotor(Motors[1], left);
+        SetMotor(Motors[2], right);
+        SetMotor(Motors[3], right);
+        Printf(" set motor to %1.2f %1.2f  \r", left, right);
         
-    }
+        ch = Getc();
+    } 
+   
+    // make sure the motors are off before exiting the demo 
+    for (i = 0; i < 4; ++i) 
+      SetMotor(Motors[i], 0);
+    Printf("\n");
 }
